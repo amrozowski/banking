e28de36153f9ff870421cb58cd888dd24d6852c1
Paweł Jastrzębski <pawel.jastrz@gmail.com>
2018-10-17 01:52:50 +0200

    Inicjalna struktura projektu Spring Boot

---
+++ src/main/java/com/staffgenics/training/banking/BankingApplication.java
@@ -0,0 +1,15 @@
+package com.staffgenics.training.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Klasa z podstawową konfiguracją aplikacji.
+ */
+@SpringBootApplication
+public class BankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankingApplication.class, args);
+  }
+}
